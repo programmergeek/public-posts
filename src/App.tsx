002ad24ebbd3973b2cf909e71580e44aa22efb42
programmergeek <programergeek@gmail.com>
2022-02-04 12:15:57 +0200

    implemented NavBar component and created MiniNavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import "./App.css";
-import {
-  LabeledIconButton,
-  PrimaryButton,
-  SecondaryButton,
-} from "./Components";
+import { MiniNavBar } from "./Components";
 import { Home } from "@styled-icons/entypo/Home";
 function App() {
   return (
     <div className="App">
-      <PrimaryButton>Read</PrimaryButton>
-      <SecondaryButton>Sparta</SecondaryButton>
-      <LabeledIconButton label="Home">
-        <Home />
-      </LabeledIconButton>
+      <MiniNavBar
+        iconsAndLabels={[
+          {
+            icon: <Home />,
+            label: "Home",
+          },
+        ]}
+      />
     </div>
   );
 }
--- src/Components/NavigationBars.tsx
+++ src/Components/NavigationBars.tsx
@@ -8,12 +8,24 @@ type navBarProps = {
   }[];
 };
 
-const navBar: React.FC<navBarProps> = ({ ...props }) => {
+const NavBar: React.FC<navBarProps> = ({ ...props }) => {
   return (
     <div className="flex flex-col bg-black rounded-full w-14 min-h-[200px]">
       {props.iconsAndLabels.map((item) => {
-        <IconButton label={item.label}>{item.icon}</IconButton>;
+        return (
+          <IconButton key={item.label} label={item.label}>
+            {item.icon}
+          </IconButton>
+        );
       })}
+    </div>
+  );
+};
+
+export const MiniNavBar: React.FC<navBarProps> = ({ ...props }) => {
+  return (
+    <div>
+      <NavBar iconsAndLabels={props.iconsAndLabels} />
     </div>
   );
 };
