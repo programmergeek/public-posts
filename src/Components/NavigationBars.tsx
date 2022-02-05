3d8a36b28db7966ace2caf7e64132ce724bfe332
programmergeek <programergeek@gmail.com>
2022-02-05 10:34:07 +0200

    increased nav bar width, added padding to the top and bottom and increased margin between buttons

--- src/Components/NavigationBars.tsx
+++ src/Components/NavigationBars.tsx
@@ -14,10 +14,10 @@ type navBarProps = {
 
 const NavBar: React.FC<navBarProps> = ({ ...props }) => {
   return (
-    <div className="flex flex-col justify-center bg-black rounded-full w-14 min-h-[200px]">
+    <div className="flex flex-col justify-center bg-black rounded-full w-16 min-h-[200px] py-8">
       {props.iconsAndLabels.map((item) => {
         return (
-          <div key={item.label} className="mb-2 ml-0.5 mt-2">
+          <div key={item.label} className="mb-3 ml-1.5 mt-3">
             <IconButton label={item.label}>{item.icon}</IconButton>
           </div>
         );
