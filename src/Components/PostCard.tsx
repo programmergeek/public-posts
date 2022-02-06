c551020976902b02947dbd0c118247984c674466
programmergeek <programergeek@gmail.com>
2022-02-06 11:50:32 +0200

    added responsive styles

--- src/Components/PostCard.tsx
+++ src/Components/PostCard.tsx
@@ -3,14 +3,14 @@ import { PrimaryButton } from "./Buttons";
 
 export const PostCard: React.FC = () => {
   return (
-    <div className="border-b-2 border-neutral-400 px-2 py-4">
+    <div className="border-b-2 border-neutral-400 px-2 py-4 sm:border-0 sm:p-5 sm:w-80 sm:h-80 sm:rounded-lg bg-white lg:w-96 lg:h-72">
       <section className="mb-1">
         <Title>Title</Title>
       </section>
       <section className="mb-4">
         <AuthorAndPublishDate author="Will Joseph" date="22 January 2022" />
       </section>
-      <section className="mb-8">
+      <section className="mb-8 sm:mb-4">
         <p className="font-openSans text-sm">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
           reiciendis obcaecati ut esse odit. Deserunt beatae libero laudantium
