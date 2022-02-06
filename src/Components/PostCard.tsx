39b50ab67a108b3296e965256d7af82614d8035a
programmergeek <programergeek@gmail.com>
2022-02-06 11:00:40 +0200

    created Author and Publish Date component

--- src/Components/PostCard.tsx
+++ src/Components/PostCard.tsx
@@ -4,10 +4,21 @@ export const PostCard: React.FC = () => {
   return (
     <div>
       <Title>Title</Title>
+      <AuthorAndPublishDate author="Will Joseph" date="22 January 2022" />
     </div>
   );
 };
 
 const Title: React.FC = ({ ...props }) => {
   return <h1 className="font-unna text-5xl"> {props.children} </h1>;
+};
+
+const AuthorAndPublishDate: React.FC<{ author: string; date: string }> = ({
+  ...props
+}) => {
+  return (
+    <p className="font-openSans text-neutral-500">
+      {props.author} • {props.date}
+    </p>
+  );
 };
