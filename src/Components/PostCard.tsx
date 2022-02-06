6fa54bb1b1767272e4f0bfc36736c33dac04e2c3
programmergeek <programergeek@gmail.com>
2022-02-06 11:16:34 +0200

    created section for post snippet and added button

--- src/Components/PostCard.tsx
+++ src/Components/PostCard.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { PrimaryButton } from "./Buttons";
 
 export const PostCard: React.FC = () => {
   return (
     <div>
       <Title>Title</Title>
       <AuthorAndPublishDate author="Will Joseph" date="22 January 2022" />
+      <p className="font-openSans">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+        reiciendis obcaecati ut esse odit. Deserunt beatae libero laudantium
+        incidunt iure facilis provident, voluptatem tempore perspiciatis
+        debitis...
+      </p>
+      <div className="flex justify-end">
+        <PrimaryButton>Read</PrimaryButton>
+      </div>
     </div>
   );
 };
