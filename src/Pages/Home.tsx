516d87e5d5923a3a4c582c08235828053cf4720b
programmergeek <programergeek@gmail.com>
2022-02-15 15:55:25 +0200

    display post cards

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MiniNavBar } from "../Components";
+import { MiniNavBar, PostCard } from "../Components";
 
 export const Home = () => {
   return (
@@ -7,7 +7,71 @@ export const Home = () => {
       <section className="hidden sm:flex sm:w-44 sm:h-screen ml-1">
         <MiniNavBar />
       </section>
-      <section></section>
+      <div className="w-[87vw] mt-7 h-11/12">
+        <div className="grid lg:grid-cols-3 2xl:grid-cols-4 gap-10 content-start h-fit place-items-center">
+          {data.map((data, key) => (
+            <div key={key} className="">
+              <PostCard
+                author={data.author}
+                postSnippet={data.postSnippet}
+                publishDate={data.publishDate}
+                title={data.title}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
+
+const data = [
+  {
+    title: "Test Title",
+    publishDate: "15 February 2022",
+    author: "Dwane Johnson",
+    postSnippet: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sed semper rutrum vestibulum. Morbi maximus leo vitae augue pulvinar, 
+        placerat vestibulum`,
+  },
+  {
+    title: "Test Title",
+    publishDate: "15 February 2022",
+    author: "Dwane Johnson",
+    postSnippet: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sed semper rutrum vestibulum. Morbi maximus leo vitae augue pulvinar, 
+        placerat vestibulum`,
+  },
+  {
+    title: "Test Title",
+    publishDate: "15 February 2022",
+    author: "Dwane Johnson",
+    postSnippet: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sed semper rutrum vestibulum. Morbi maximus leo vitae augue pulvinar, 
+        placerat vestibulum`,
+  },
+  {
+    title: "Test Title",
+    publishDate: "15 February 2022",
+    author: "Dwane Johnson",
+    postSnippet: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sed semper rutrum vestibulum. Morbi maximus leo vitae augue pulvinar, 
+        placerat vestibulum`,
+  },
+  {
+    title: "Test Title",
+    publishDate: "15 February 2022",
+    author: "Dwane Johnson",
+    postSnippet: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sed semper rutrum vestibulum. Morbi maximus leo vitae augue pulvinar, 
+        placerat vestibulum`,
+  },
+  {
+    title: "Test Title",
+    publishDate: "15 February 2022",
+    author: "Dwane Johnson",
+    postSnippet: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sed semper rutrum vestibulum. Morbi maximus leo vitae augue pulvinar, 
+        placerat vestibulum`,
+  },
+];
