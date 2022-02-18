89c9c55553892378e65fd08d067df850fe8bce89
programmergeek <programergeek@gmail.com>
2022-02-18 16:04:53 +0200

    created method to seperate text into different paragraphs

--- src/Components/PostContent.tsx
+++ src/Components/PostContent.tsx
@@ -4,3 +4,24 @@ import React from "react";
 export const PostContent = ({ ...props }) => {
   return [<p key={1}></p>];
 };
+
+/** Separates text into paragraphs.
+ * Return an array of the paragraphs.
+ */
+const processText = (text: string) => {
+  // array to store separated paragraphs
+  const textArr = [] as string[];
+  while (text.search("\n") !== -1) {
+    // look for the index of the new line
+    const index = text.search("\n");
+    // copy everything before the new line
+    const paragraph = text.substring(0, index + 1);
+    // add the copy to the array
+    textArr.push(paragraph);
+    // delete the copied text
+    text = text.substring(index + 2);
+  }
+  // add the final paragraph to the array
+  textArr.push(text);
+  return textArr;
+};
